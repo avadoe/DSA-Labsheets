c5d09ca8c3ef94cf66e0b9c2e5ac2d83a063595c
avadoe <aashuaripirala@gmail.com>
2023-03-30 13:36:20 +0530

    B

--- Labsheet 8/questionB.cpp
+++ Labsheet 8/questionB.cpp
@@ -1,40 +1,80 @@
 #include <iostream>
-#include <vector>
 using namespace std;
+
+string nextPalindrome(string num){
+    int n = num.size();
+    bool allNines = true;
+
+    for(int i = 0; i < n; i++){
+        if(num[i] != '9'){
+            allNines = false;
+            break;
+        }
+    }
+
+    if(allNines){
+        string ans(n + 1, '0');
+        ans[0] = '1';
+        ans[n] = '1';
+
+        return ans;
+    }
+
+    int mid = n / 2;
+    int i = mid - 1;
+    int j = (n % 2 == 0) ? mid : mid + 1;
+
+    while(i >= 0 && num[i] == num[j]){
+        i--;
+        j++;
+    }
+
+    bool leftSmaller = false;
+    if(i < 0 || num[i] < num[j])
+        leftSmaller = true;
+
+    while(i >= 0){
+        num[j] = num[i];
+        i--;
+        j++;
+    }
+
+    if(leftSmaller){
+        int carry = 1;
+        i = mid - 1;
+
+        if(n % 2 == 1){
+            int x = num[mid] - '0' + carry;
+            carry = x / 10;
+            num[mid] = (x % 10) + '0';
+            j = mid + 1;
+        }
+        else{
+            j = mid;
+        }
+
+        while(i >= 0){
+            int x = num[mid] - '0' + carry;
+            carry = x / 10;
+            num[mid] = (x % 10) + '0';
+            num[j] = num[i];
+            i--;
+            j++;
+        }
+    }
+
+    return num;
+}
+
 int main()
 {
     int n;
     cin >> n;
-    vector<int> number(n);
 
-    string A;
-    cin >> A;
+    string num;
+    cin >> num;
 
-    for(int i = 0; i < n; i++){
-        char c = A[i];
-
-        number[i] = c - '0';
-    }
-
-    int left, right;
-
-    if(n % 2 == 0){
-        left = n / 2 - 1;
-        right = n / 2;
-    }else{
-        left = n / 2;
-        right = n / 2;
-    }
-
-    while(left >= 0 && number[left] == number[right]){
-        left--;
-        right++;
-    }
-
-    if(left > 0 && number[left] > number[right]){
-        bool isEven = (n % 2 == 0);
-        int carry = 1;
-
-        number[left] += carry;
-    }
+    string ans = nextPalindrome(num);
+    cout << ans << endl;
+    return 0;
 }
