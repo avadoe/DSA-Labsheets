cfa6a119a43c4edd291f739e3f080018d9dee3bc
avadoe <aashuaripirala@gmail.com>
2023-04-05 01:32:59 +0530

    Lab8 done

--- Labsheet 8/questionD.cpp
+++ Labsheet 8/questionD.cpp
@@ -1,50 +1,38 @@
-// WRONG
-
 #include <iostream>
 #include <vector>
-#include <set>
 using namespace std;
+
 int main()
 {
-    long long n, q;
+    int n, q;
     cin >> n >> q;
 
-    vector<long long> arr(n);
+    vector<int> a(n);
 
-    while(q--){
-        long long type, k;
+    for(int i = 0; i < q; i++){
+        int type, k;
+
         cin >> type >> k;
 
         if(type == 1){
-            arr[k - 1] ^= 1;
+            a[k - 1] = 1 - a[k - 1];
         }
-
         else if(type == 2){
-            long long ans = -1;
-
-            for(long long i = k - 1; i < n; i++){
-                if(arr[i] == 1){
-                    ans = i + 1;
-                    break;
-                }
+            auto it = find(a.begin() + k - 1, a.end(), 1);
+            if(it != a.end()){
+                cout << distance(a.begin(), it) + 1 << endl;
             }
-
-            cout << ans << endl;
+            else{
+                cout << -1 << endl;
+            }
         }
-
         else{
-            long long ans = -1;
-
-            for(long long i = k - 1; i >= 0; i--){
-                if(arr[i] == 1){
-                    ans = i + 1;
-                    break;
-                }
+            auto rit = find(a.rbegin() + (n - k), a.rend(), 1);
+            if (rit != a.rend()) {
+                cout << n - distance(a.rbegin(), rit) << endl;
+            } else {
+                cout << -1 << endl;
             }
-
-            cout << ans << endl;
         }
     }
-
-    return 0;
 }
