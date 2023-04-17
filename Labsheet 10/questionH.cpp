f418e3e5f769fe1d90b9c0ac520cfc13207e5dc0
avadoe <aashuaripirala@gmail.com>
2023-04-17 22:33:46 +0530

    E using array

--- Labsheet 10/questionH.cpp
+++ Labsheet 10/questionH.cpp
@@ -1,6 +1,7 @@
 // WRONG
 
 #include <iostream>
+#include <queue>
 #include <unordered_map>
 #include <vector>
 using namespace std;
@@ -9,38 +10,46 @@ int main()
     int n, m;
     cin >> n >> m;
 
-    vector<int> a(n);
-    vector<int> b(m);
-
+    vector<int> a(n), b(m);
     for(int i = 0; i < n; i++)
         cin >> a[i];
 
     for(int i = 0; i < m; i++)
         cin >> b[i];
 
-    unordered_map<int, int> a_count;
-    unordered_map<int, int> positions;
+    unordered_map<int, int> freq;
 
-    for(int i = 0; i < m; i++)
-        positions[b[i]] = i;
+    for(int i = 0; i < n; i++){
+        freq[a[i]]++;
+    }
 
-    for(int i = 0; i < n; i++)
-        a_count[a[i]]++;
-
-    vector<int> ans(n);
-
-    int start = 0;
-    for(int i = 0; i < m; i++){  
-        if(a_count[b[i]]){
-            while(a_count[b[i]]){
+    queue<int> q;
+    vector<int> ans;
+    for(int i = 0; i < m; i++){
+        if(freq[b[i]] != 0){
+            while(freq[b[i]]){
                 ans.push_back(b[i]);
-                a_count[b[i]]--;
+                freq[b[i]]--;
             }
+        }else{
+            q.push(b[i]);
         }
     }
 
-    for(int i = 0; i < n; i++)
+    vector<int> test;
+    while(!q.empty()){
+        test.push_back(q.front());
+        q.pop();
+    }
+
+    sort(test.begin(), test.end());
+    for(int i = 0; i < test.size(); i++){
+        ans.push_back(test[i]);
+    }
+
+    for(int i = 0; i < n; i++){
         cout << ans[i] << " ";
+    }
 
     cout << "\n";
     return 0;
--- Labsheet 9/questionE.cpp
+++ Labsheet 9/questionE.cpp
@@ -1,37 +1,58 @@
-// NOT CONFIRMED BUT ANSWER IS CORRECT
-// NEED TO CHECK TIME COMPLEXITY
-
 #include <iostream>
-#include <vector>
 using namespace std;
-
-int main() {
+int main()
+{
     int m, k, n;
     cin >> m >> k >> n;
 
     int p;
     cin >> p;
-    vector<vector<int> > mat1(m, vector<int>(k, 0));
-    for (int i = 0; i < p; i++) {
+    int m1[m][k];
+    int m2[k][n];
+
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < k; j++){
+            m1[i][j] = 0;
+        }
+    }
+
+    for(int i = 0; i < k; i++){
+        for(int j = 0; j < n; j++){
+            m2[i][j] = 0;
+        }
+    }
+
+    for(int i = 0; i < p; i++){
         int row, col, val;
+
         cin >> row >> col >> val;
-        mat1[row - 1][col - 1] = val; 
+
+        m1[row - 1][col - 1] = val;
     }
 
     int q;
     cin >> q;
-    vector<vector<int> > mat2(k, vector<int>(n, 0));
-    for (int i = 0; i < q; i++) {
+
+    for(int i = 0; i < q; i++){
         int row, col, val;
+
         cin >> row >> col >> val;
-        mat2[row - 1][col - 1] = val; 
+
+        m2[row - 1][col - 1] = val;
     }
 
-    vector<vector<int> > mat3(m, vector<int>(n, 0));
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            for (int p = 0; p < k; p++) {
-                mat3[i][j] += mat1[i][p] * mat2[p][j];
+    int m3[m][n];
+
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            m3[i][j] = 0;
+        }
+    }
+
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            for(int x = 0; x < k; x++){
+                m3[i][j] += m1[i][x] * m2[x][j];
             }
         }
     }
@@ -39,21 +60,20 @@ int main() {
     int count = 0;
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
-            if (mat3[i][j] != 0) {
+            if (m3[i][j] != 0) {
                 count++;
             }
         }
     }
-    
+
     cout << count << endl;
 
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
-            if (mat3[i][j] != 0) {
-                cout << i+1 << " " << j+1 << " " << mat3[i][j] << endl; 
+            if (m3[i][j] != 0) {
+                cout << i+1 << " " << j+1 << " " << m3[i][j] << endl; 
             }
         }
     }
-
     return 0;
 }
