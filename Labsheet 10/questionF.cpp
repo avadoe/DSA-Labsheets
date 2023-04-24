608e53274799f550c73d3bb45125fe29a3a68c97
avadoe <aashuaripirala@gmail.com>
2023-04-25 00:26:53 +0530

    F

--- Labsheet 10/questionF.cpp
+++ Labsheet 10/questionF.cpp
@@ -1,29 +1,49 @@
-// DONE
-
 #include <iostream>
 #include <vector>
-#include <map>
 using namespace std;
-int main()
-{
+
+long long merge(int arr[], int temp[], int left, int mid, int right) {
+    long long inversions = 0;
+    int i = left, j = mid, k = left;
+    while (i < mid && j <= right) {
+        if (arr[i] <= arr[j]) {
+            temp[k++] = arr[i++];
+        } else {
+            temp[k++] = arr[j++];
+            inversions += mid - i;
+        }
+    }
+    while (i < mid) {
+        temp[k++] = arr[i++];
+    }
+    while (j <= right) {
+        temp[k++] = arr[j++];
+    }
+    for (int i = left; i <= right; i++) {
+        arr[i] = temp[i];
+    }
+    return inversions;
+}
+
+long long mergeSort(int arr[], int temp[], int left, int right) {
+    long long inversions = 0;
+    if (left < right) {
+        int mid = (left + right) / 2;
+        inversions += mergeSort(arr, temp, left, mid);
+        inversions += mergeSort(arr, temp, mid + 1, right);
+        inversions += merge(arr, temp, left, mid + 1, right);
+    }
+    return inversions;
+}
+
+int main() {
     int n;
     cin >> n;
-
-    vector<int> arr(n);
-    for(int i = 0; i < n; i++)
+    int arr[n], temp[n];
+    for (int i = 0; i < n; i++) {
         cin >> arr[i];
-
-    map<int, int> mp;
-
-    int count = 0;
-
-    for(int i = 0; i < n; i++){
-        auto it = mp.upper_bound(arr[i]);
-        while(it != mp.end()){
-            count += it->second;
-            it++;
-        }
-        mp[arr[i]]++;
     }
-    cout << count << endl;
+    long long inversions = mergeSort(arr, temp, 0, n - 1);
+    cout << inversions << endl;
+    return 0;
 }
