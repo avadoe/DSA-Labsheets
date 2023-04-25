c02345c18d35df0be5a131b4837946025dc59053
avadoe <aashuaripirala@gmail.com>
2023-04-25 15:00:25 +0530

    Done

--- Labsheet 11/questionA.cpp
+++ Labsheet 11/questionA.cpp
@@ -1,4 +1,4 @@
-// Segmentation fault for large testcases
+// DONE
 
 #include <iostream>
 #include <vector>
@@ -8,32 +8,19 @@ int main()
     long long n, w;
     cin >> n >> w;
 
-    vector<pair<long long, long long> > w_c(n);
-    for(int i = 0; i < n; i++){
-        long long x, y;
-        cin >> x >> y;
-
-        w_c[i].first = x;
-        w_c[i].second = y;
+    vector<pair<long long, long long> > arr(n);
+    for(long long i = 0; i < n; i++){
+        cin >> arr[i].first >> arr[i].second;
     }
 
-    long long dp[n + 1][w + 1];
-
-    for(long long i = 0; i <= n; i++){
-        for(long long j = 0; j <= w; j++){
-            dp[i][j] = 0;
-        }
-    }
+    vector<vector<long long> > dp(n + 1, vector<long long> (w + 1, 0));
 
     for(long long i = 1; i <= n; i++){
-        long long wi = w_c[i - 1].first;
-        long long ci = w_c[i - 1].second;
-
         for(long long j = 1; j <= w; j++){
-            if(j >= wi){
-                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - wi] + ci);
+            if(j >= arr[i - 1].first){
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - arr[i - 1].first] + arr[i - 1].second);
             }
-            else{
+            else {
                 dp[i][j] = dp[i - 1][j];
             }
         }
@@ -41,4 +28,4 @@ int main()
 
     cout << dp[n][w] << endl;
     return 0;
-}
+}   
